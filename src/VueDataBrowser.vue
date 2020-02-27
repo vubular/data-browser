401bf8be7bbe8f2e9ec5ba5a3fcc41be7664a500
Mentor Gashi <m.gashi@albismart.com>
2020-02-27 17:01:21 +0100

    Basic refactoring

---
+++ src/VueDataBrowser.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>
+		<h3> Test </h3>
+	</div>
+</template>
+<script>
+	import ItemBrowser from "./item/ItemBrowser.vue";
+	import ListBrowser from "./item/ItemBrowser.vue";
+	export default {
+		name: "VueDataBrowser",
+		components: { ItemBrowser, ListBrowser },
+		props: {
+			data: {
+				type: [Object, Array],
+				required: true
+			},
+			view: {
+				type: String,
+				default: "table"
+			}
+		}
+	}
+</script>
--- src/vue-data-browser.vue
+++
@@ -1,7 +0,0 @@
-<template>
-	<div class="test">
-		Hello!
-	</div>
-</template>
-<script src="./vue-data-browser.js"></script>
-<style src="./vue-data-browser.css"></style>
