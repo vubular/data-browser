cf5d5ce9ac9e408da436ff6ca7a8299374e6f931
ZgjimDida <zgjimdida55@hotmail.com>
2020-03-03 14:35:45 +0100

    Export VueDataBrowser component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 // Import vue component
 import component from './VueDataBrowser.vue';
 
-// Declare install function executed by Vue.use()
-export default {
-	install(Vue) {
-		Vue.component('VueDataBrowser', component);
-	}
+export {
+	VueDataBrowser as VueDataBrowser
 }
