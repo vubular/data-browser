d80bfc0def9260b322603330d6f75134e399c5f9
ZgjimDida <zgjimdida55@hotmail.com>
2020-03-23 13:48:07 +0100

    Show/Hide columns in Grid and List view

--- src/list/views/GridView.vue
+++ src/list/views/GridView.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="columns is-multiline">
 		<div class="column is-3 section" v-for="(value, key) in data" :key="key">
-			<span v-for="(propValue, propName) in value" :key="propName" class="item">
+			<span v-for="(propValue, propName) in value" :key="propName" class="item" v-show="columnNames[propName]" >
 				<span class="has-text-weight-bold">{{propName}}:</span><br/>
 				<span class="value">{{propValue}}</span><br/>
 			</span>
@@ -14,6 +14,9 @@
 		props: {
 			data: {
 				type: Array
+			},
+			columnNames: {
+				type: Object
 			}
 		},
 	}
--- src/list/views/ListView.vue
+++ src/list/views/ListView.vue
@@ -2,7 +2,7 @@
 	<div>
 		<ul class="list">
 			<li v-for="(value, key) in data" :key="key" class="section">
-				<span v-for="(propValue, propName) in value" :key="propName">
+				<span v-for="(propValue, propName) in value" :key="propName" v-show="columnNames[propName]" >
 					<span><span class="has-text-weight-bold">{{propName}}:</span> {{propValue}}</span><br/>
 				</span>
 			</li>
@@ -20,6 +20,9 @@ ul.list li{
 		props: {
 			data: {
 				type: Array
+			},
+			columnNames: {
+				type: Object
 			}
 		},
 	}
