b712b2cdf7d8cae2428f79d1dd2d83c337bbc6bc
Mentor Gashi <m.gashi@albismart.com>
2020-04-09 17:52:03 +0200

    Initial default slot Show and Edit route

--- src/list/views/TableView.vue
+++ src/list/views/TableView.vue
@@ -12,11 +12,20 @@
 				<tr v-for="(item, i) in data" :key="i">
 					<th v-if="fields.includes('+')" class="has-text-grey-light has-text-weight-light">{{i+1}}</th>
 					<td v-if="fields.includes(propName)" v-for="(propValue, propName) in item" :key="propName">
-						{{propValue}}
+						<template v-if="propName=='name'">
+							<router-link :to="$route.fullPath+'/'+item.id">{{propValue}}</router-link>
+						</template>
+						<template v-else>{{propValue}}</template>
 					</td>
 					<td class="is-narrow">
 						<slot name="actions" v-bind:item="item">
-							<button class="button is-danger is-outlined"><i class="fa fa-trash"></i></button>
+							<router-link :to="$route.fullPath+'/'+item.id+'/edit'" class="button is-info is-outlined is-small">
+								<span class="icon"><i class="fa fa-edit"></i></span>
+								<span>Edit</span>
+							</router-link>
+							<button class="button is-danger is-outlined is-small" style="margin-left:10px">
+								<span class="icon"><i class="fa fa-trash"></i></span>
+							</button>
 						</slot>
 					</td>
 				</tr>
@@ -35,11 +44,6 @@
 			archiveItem(item) {
 				//dialog
 				this.$emit("archived", item);
-			}
-		},
-		computed: {
-			anySelected(){
-				return Object.values(this.columnNames).includes(true);
 			}
 		}
 	}
