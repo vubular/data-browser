35c7592e025bd1f30e7c3f5731ca62f87012e670
Mentor Gashi <m.gashi@albismart.com>
2020-04-09 17:50:16 +0200

    Preloader, actions prop, watching when data is passed, altering initial filtered items

--- src/list/ListBrowser.vue
+++ src/list/ListBrowser.vue
@@ -1,12 +1,14 @@
 <template>
 	<div class="list-browser" style="margin-top:30px">
 		<div class="box is-paddingless">
-			<actions @search="searching" v-on="$listeners"></actions>
+			<actions :actions="actions" @search="searching" v-on="$listeners"></actions>
 			<!-- <filters></filters> -->
-			<div class="listing-items">
+			<div class="listing-items is-relative">
 				<component v-bind:is="viewMode"
-					:data="filteredList"
-					:fields="fields"></component>
+					:data.sync="filteredItems"
+					:fields="fields"
+					:key="filteredItems.length"></component>
+				<b-loading :is-full-page="false" :active.sync="UIPending"></b-loading>
 			</div>
 		</div>
 	</div>
@@ -23,9 +25,15 @@
 		name: "ListBrowser",
 		components: { Actions, Filters, ListView, TableView, GridView },
 		props: {
+			actions: { type: String },
+			view: { type: String },
 			data: { type: Array },
-			fields: { type: String },
-			view: { type: String }
+			fields: { type: String }
+		},
+		data() {
+			return {
+				filteredItems: []
+			}
 		},
 		computed: {
 			viewMode() {
@@ -33,45 +41,36 @@
 				if(this.view == 'list') view = "list-view";
 				if(this.view == 'grid') view = "grid-view";
 				return view;
-			},
-			filteredList(){
-				if(this.search===''){
-					return this.data;
-				} else {
-					if(this.searchField===''){
-						return this.data.filter(item => {
-							let show = false
-							Object.values(item).forEach(value => {
-								if(value && typeof value === 'string' && value.toLowerCase().includes(this.search.toLowerCase())){
-									show = true;
-								}
-								if(value && typeof value === 'number' && value===+this.search){
-									show = true;
-								}
-							})
-							return show;
-						});
-					} else {
-						return this.data.filter(item => {
-							if(item[this.searchField]) {
-								return item[this.searchField].toLowerCase().includes(this.search.toLowerCase());
-							}
-						})
-					}
-
-				}
 			}
 		},
 		methods: {
-			searching(val) {
-				console.log(val);
-				this.search = val;
+			searching(value) {
+				console.log("Test");
+				clearTimeout(filterItems);
+				var filterItems = setTimeout(() => {
+					if(value===''){
+						this.filteredItems = this.data;
+					} else {
+						this.filteredItems = [];
+						this.data.filter(item => {
+							Object.values(item).forEach(i => {
+								if(i && typeof i === 'string' && i.toLowerCase().includes(value.toLowerCase())) {
+									if(this.filteredItems.indexOf(item)===-1) this.filteredItems.push(item);
+								}
+								if(i && typeof i === 'number' && i===+value){
+									if(this.filteredItems.indexOf(item)===-1) this.filteredItems.push(item);
+								}
+							});
+						});
+					}
+				}, 250);
 			}
 		},
-		data() {
-			return {
-				search: '',
-				searchField: ''
+		watch: {
+			data(newData, oldData) {
+				if(newData && newData!=oldData) {
+					this.filteredItems = newData;
+				}
 			}
 		}
 	}
